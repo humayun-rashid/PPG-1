d5e0067dabf1b84f20469b8781ff4d738a1e9e24
humayun-rashid <47626698+humayun-rashid@users.noreply.github.com>
2019-03-21 00:01:47 +0200

    Update Sensor_node_1_(PPG_1).ino

--- Sensor_node_1_(PPG_1).ino
+++ Sensor_node_1_(PPG_1).ino
@@ -31,7 +31,7 @@ void setup()
   // Initialize sensor
   if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
   {
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    Serial.println("PPG Sensor to colelct heart rate");
     while (1);
   }
   Serial.println();
